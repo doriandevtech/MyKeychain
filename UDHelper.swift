7b6848e10d7fcc1de28a121cbd0513e0b85d8154
dodoleouf <61510923+dodoleouf@users.noreply.github.com>
2023-05-05 15:49:39 +0200

    Update: Getter & Setters for different fields (url, user, pwd) 👷🏻‍♂️

--- UDHelper.swift
+++ UDHelper.swift
@@ -16,10 +16,42 @@ class UDHelper {
     private let itemUserKey = "UserKey"
     private let itemPwdKey = "PwdKey"
     
-    func getArray() -> [String] {
+    func getUrl() -> [String] {
+        if let array = userDefaults.array(forKey: itemUrlKey) as? [String] {
+            return array
+        }
+        return []
+    }
+    
+    func setUrl(_ newValue: String) {
+        var a = getUrl()
+        a.append(newValue)
+        userDefaults.set(a, forKey: itemUrlKey)
+    }
+    
+    func getUser() -> [String] {
         if let array = userDefaults.array(forKey: itemUserKey) as? [String] {
             return array
         }
         return []
+    }
+    
+    func setUser(_ newValue: String) {
+        var a = getUser()
+        a.append(newValue)
+        userDefaults.set(a, forKey: itemUserKey)
+    }
+    
+    func getPwd() -> [String] {
+        if let array = userDefaults.array(forKey: itemPwdKey) as? [String] {
+            return array
+        }
+        return []
+    }
+    
+    func setPwd(_ newValue: String) {
+        var a = getPwd()
+        a.append(newValue)
+        userDefaults.set(a, forKey: itemPwdKey)
     }
 }
